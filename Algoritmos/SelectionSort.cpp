301b97ad05ff9ca0e1f5f798297d20de44a67d83
JIMMY ANDRES MENDIETA RIVERA <jamendietar@itc.edu.co>
2022-07-18 08:40:13 -0500

    selection sort

---
+++ Algoritmos/SelectionSort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int posicion_minima(const vector<int>& v, int left, int right) {
+	int posicion = left;
+	for (int i = left+1; i <= right; i++)
+	{
+		if (v[i] < v[posicion]) posicion = i;
+	}
+	return posicion;
+}
+
+void selection_sort(vector<int>& v) {
+	int last = v.size() - 1;
+	for (int i = 0; i < last; i++)
+	{
+		int k = posicion_minima(v, i, last);
+		swap(v[k], v[i]);
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	cout << "El vector sin ordenar es: " << endl;
+	for (int j = 0; j < n; j++)
+	{
+		cout << v[j] << ' ';
+	}
+
+	cout << endl;
+
+	selection_sort(v);
+
+	cout << "El vector ordenado es: " << endl;
+	for (int k = 0; k < n; k++)
+	{
+		cout << v[k] << ' ';
+	}
+}
